63c2efbd00b1a9268c2738f895b909a4531d0d54
Richard Pickett <Richard@RichardPickett.com>
2022-09-06 16:43:37 -0500

    walking through comments

--- index.js
+++ index.js
@@ -1,18 +1,49 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
-// issue object is defined here: https://docs.github.com/en/graphql/reference/objects#issue
-var success = true;
+/*
 
-try {
-  // issue input defined in action metadata file
-  const issue = core.getInput("issue");
-  console.log("the issue passed by the workflow:", issue);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log("The event payload:", payload);
-} catch (error) {
-  success = false;
-  core.setFailed(error.message);
+issue object is defined here: https://docs.github.com/en/graphql/reference/objects#issue
+
+octokit/rest.js API: https://octokit.github.io/rest.js/v18
+
+example that walks through all the comments: https://github.com/peter-evans/find-comment
+
+*/
+
+async function run() {
+  var success = true;
+
+  try {
+    const issue = core.getInput("issue");
+    const githubToken = core.getInput("githubToken");
+    const octokit = github.getOctokit(githubToken);
+    console.log("the issue passed by the workflow:", JSON.stringify(issue, undefined, 2));
+
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = github.context.payload;
+    console.log("The event payload:", JSON.stringify(payload, undefined, 2));
+
+    const repository = core.getInput("repository");
+    const [owner, repo] = repository.split("/");
+
+    // get the issue and walk through the comments
+    const parameters = {
+      owner,
+      repo,
+      issue_number: payload.issue.id,
+    };
+
+    for await (const comments of octokit.paginate.iterator(octokit.rest.issues.listComments, parameters)) {
+      comments.forEach((comment) => {
+        console.log("COMMENT: ", comment);
+      });
+    }
+  } catch (error) {
+    success = false;
+    core.setFailed(error.message);
+  }
+  core.setOutput("success", success);
 }
-core.setOutput("success", success);
+
+await run();
